020f00fa18b0fe76cefd49c11320968186fb26f6
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2024-03-11 11:42:42 -0700

    fixed nav scroll down issue

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,10 @@ const menu = document.querySelector('.bottom-nav');
 function openNav() {
     if (menu.classList.contains("showMenu")) {
         menu.classList.remove("showMenu");
-        menu.style.display = "none";
+        // menu.style.display = "none";
       } else {
         menu.classList.add("showMenu");
-        menu.style.display = "flex";
+        // menu.style.display = "flex";
       }
 }
 
