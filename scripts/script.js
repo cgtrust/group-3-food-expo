1657736d33dda91b01a31fba947b486415abd4eb
ninaweng6716 <146882360+ninaweng6716@users.noreply.github.com>
2024-03-11 18:42:18 -0700

    finished media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// nav open/close function in mobile
 const btnNav = document.querySelector('.nav-button');
 const menu = document.querySelector('.bottom-nav');
 
@@ -12,3 +13,30 @@ function openNav() {
 }
 
 btnNav.addEventListener('click', openNav);
+
+// move #socials out of its div for media query purposes
+const socials = document.getElementById('socials');
+const topNav = document.querySelector('.top-nav');
+const bottomNav = document.querySelector('.bottom-nav');
+// const search = document.querySelector('.search-bar');
+
+function moveSearch() {
+  if(window.innerWidth >= 500) {
+    topNav.appendChild(search);
+  } else if (window.innerWidth < 500) {
+    bottomNav.appendChild(search);
+  }
+}
+
+function moveSocials() {
+  if(window.innerWidth >= 500) {
+    topNav.appendChild(socials);
+  } else if (window.innerWidth < 500) {
+    bottomNav.appendChild(socials);
+  }
+}
+
+window.addEventListener('resize', moveSocials, moveSearch);
+
+moveSocials();
+// moveSearch();
